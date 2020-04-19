0e2d27b7de9ce9a37b606c42b0e143d1e48a7052
omagana <omagana@gmail.com>
2020-04-19 13:20:59 -0500

    agregando comentarios

--- src/app/header/hcomponent/hcomponen.component.ts
+++ src/app/header/hcomponent/hcomponen.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'pkd-hcomponen',
+  selector: 'pkd-hcomponen',//este es el que se jala para el "main.htlm"
   templateUrl: './hcomponen.component.html',
   styleUrls: ['./hcomponen.component.scss']
 })
